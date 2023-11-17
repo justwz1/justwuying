835c289ea0d08a3fdf062c3410c87abb7eb79cb9
wuzhen <wuzhen>
2023-11-17 15:20:19 +0800

    子应用

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# 安装子应用
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # book ， 方案1
+    # 'book.apps.BookConfig'  # 方案2
+
 ]
 
 MIDDLEWARE = [
