ddd9a4adcd4b743f26641d68d0097350eda6aa5c
wuzhen <wuzhen>
2023-11-19 22:13:24 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -11,11 +11,22 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse
 
+
 # 我们期望用户输入 http://127.0.0.1:8000/index/
 # 来访问视图函数
 
 def index(request):
+    # return HttpResponse('ok')
 
-    return HttpResponse('ok')
+    # render 渲染模板
+    # request, template_name, context = None,
+    # request,         请求
+    # template_name,   模板名字
+    # context = None
 
+    # 模拟数据查询
+    context = {
+        'name': '马上双11, 点击有惊喜'
+    }
 
+    return render(request, 'book/index.html', context=context)
