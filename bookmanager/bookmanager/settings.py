850606edcf19b5d8fe6bf583e7e1c253ca4e17c0
wuzhen <wuzhen>
2023-11-20 21:31:02 +0800

    静态资源

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # print(__file__)
 
 
@@ -28,8 +27,7 @@ SECRET_KEY = 'i%gl)gm9@c*%@9p%zvy1=o^z*62unnp)hs69d$=r*p+$b0mi3*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 # 安装子应用
@@ -77,7 +75,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bookmanager.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -88,7 +85,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -108,11 +104,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 # 设置语言
-LANGUAGE_CODE = 'zh-Hans'#'en-us'
+LANGUAGE_CODE = 'zh-Hans'  # 'en-us'
 # 设置时区
 TIME_ZONE = 'UTC'
 
@@ -122,8 +117,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# 告知系统 我们的图片在那里
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
